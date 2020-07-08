90701db99a2fac35e6469d10697d326f37228791
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-07-08 16:49:47 +0100

    test: pass functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,10 +7,14 @@
  */
 
 const chaiHttp = require("chai-http");
+const { before } = require("mocha");
 const chai = require("chai");
 
-const { assert } = chai;
 const server = require("../server");
+const Book = require("../models/Book");
+
+const { assert } = chai;
+const GLOBALS = {};
 
 chai.use(chaiHttp);
 
@@ -19,6 +23,27 @@ suite("Functional Tests", function() {
    * ----[EXAMPLE TEST]----
    * Each test should completely test the response of the API end-point including response status code!
    */
+
+  before(async () => {
+    const bookTitles = ["Test Title 1", "Test Title 2", "Test Title 3"];
+
+    const bookPromisesToResolve = bookTitles.map((title) => {
+      const newBook = new Book({ title });
+      return newBook.save();
+    });
+
+    const booksInDb = await Promise.all(bookPromisesToResolve);
+
+    GLOBALS._id = booksInDb[0]._id;
+    GLOBALS.oldCommentCount = booksInDb[0].comments.length;
+  });
+
+  after((done) => {
+    Book.deleteMany({})
+      .then(() => done())
+      .catch((err) => done(err));
+  });
+
   test("#example Test GET /api/books", function(done) {
     chai
       .request(server)
@@ -53,28 +78,75 @@ suite("Functional Tests", function() {
       "POST /api/books with title => create book object/expect book object",
       function() {
         test("Test POST /api/books with title", function(done) {
-          // done();
+          chai
+            .request(server)
+            .post("/api/books")
+            .send({ title: "test title" })
+            .end((err, res) => {
+              GLOBALS._id = res.body._id;
+
+              assert.equal(res.status, 200);
+              assert.property(res.body, "_id");
+              assert.property(res.body, "title");
+              assert.property(res.body, "comments");
+              assert.isArray(res.body.comments);
+              done();
+            });
         });
 
         test("Test POST /api/books with no title given", function(done) {
-          // done();
+          chai
+            .request(server)
+            .post("/api/books")
+            .send({})
+            .end((err, res) => {
+              assert.equal(res.status, 400);
+              assert.propertyVal(res.body, "error", "missing title");
+              done();
+            });
         });
       }
     );
 
     suite("GET /api/books => array of books", function() {
       test("Test GET /api/books", function(done) {
-        // done();
+        chai
+          .request(server)
+          .get("/api/books")
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isArray(res.body);
+            assert.property(res.body[0], "commentcount");
+            assert.property(res.body[0], "title");
+            assert.property(res.body[0], "_id");
+            done();
+          });
       });
     });
 
     suite("GET /api/books/[id] => book object with [id]", function() {
       test("Test GET /api/books/[id] with id not in db", function(done) {
-        // done();
+        chai
+          .request(server)
+          .get("/api/books/0123456789ab")
+          .end((err, res) => {
+            assert.equal(res.status, 404);
+            done();
+          });
       });
 
       test("Test GET /api/books/[id] with valid id in db", function(done) {
-        // done();
+        chai
+          .request(server)
+          .get(`/api/books/${GLOBALS._id}`)
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.property(res.body, "_id");
+            assert.property(res.body, "title");
+            assert.property(res.body, "comments");
+            assert.isArray(res.body.comments);
+            done();
+          });
       });
     });
 
@@ -82,7 +154,23 @@ suite("Functional Tests", function() {
       "POST /api/books/[id] => add comment/expect book object with id",
       function() {
         test("Test POST /api/books/[id] with comment", function(done) {
-          // done();
+          const { _id, oldCommentCount } = GLOBALS;
+          const testComment = "spam and eggs is yum";
+
+          chai
+            .request(server)
+            .post(`/api/books/${_id}`)
+            .send({ comment: testComment })
+            .end((err, res) => {
+              assert.equal(res.status, 200);
+              assert.property(res.body, "_id");
+              assert.property(res.body, "title");
+              assert.property(res.body, "comments");
+              assert.isArray(res.body.comments);
+              assert.equal(res.body.comments.length, oldCommentCount + 1);
+              assert.equal(res.body.comments.includes(testComment), true);
+              done();
+            });
         });
       }
     );
